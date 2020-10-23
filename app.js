81e97e39e786b6c094f54ffbb98ff4ebdc538a3e
Jake Simmens <rocknjake@gmail.com>
2020-10-23 11:11:00 -0500

    Initial commit with basic book shelf

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get('/', function getHome(req, res) {
+    //res.send("It's working!");
+    console.log("Get home page");
+    res.render("home");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function startServer() {
+    console.log("Book server is up and running");
+});
