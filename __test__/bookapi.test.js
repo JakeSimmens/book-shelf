777478b7267df8941a53573ccdbeadbcafcb78b1
Jake Simmens <rocknjake@gmail.com>
2020-11-04 16:15:38 -0600

    Access data from Google Books using a word search

--- __test__/bookapi.test.js
+++ __test__/bookapi.test.js
@@ -1,10 +1,13 @@
-const { TestScheduler } = require("jest");
-const Book = require("../public/js/bookapi.js");
+const { Book } = require("../public/js/bookapi.js");
 
 describe("Book module", () => {
     let book = Book();
 
     test("should return an array of 0 to 5 items", () => {
-        expect(book.search("Jurassic Park")).toBeLessThanOrEqual(5);
+        expect(book.search("Jurassic Park").length).toBeLessThanOrEqual(5);
     });
 });
+
+// test("should return 5", () => {
+//     expect(five()).toBe(5);
+// });
--- public/js/bookapi.js
+++ public/js/bookapi.js
@@ -1,56 +1,46 @@
-const express = require("express");
-const fetch = require("node-fetch");
+const searchInput = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const searchResults = document.querySelector(".searchResults");
 
-//search for book
+
+searchBtn.addEventListener("click",
+    function seachBtnClick() {
+        searchSubject(searchInput.value);
+    });
+
+
+const googleBookQueryStr = "https://www.googleapis.com/books/v1/volumes?q=";
+
+function searchSubject(term) {
+    if (term == "") {
+        return;
+    }
+
+    fetch(`${googleBookQueryStr}${term}`)
+        .then(response => response.json())
+        .then(data => displaySearchResults(data));
+}
+
+function displaySearchResults(data) {
+    console.log("DATA");
+    console.log(data);
+
+    for (let book of data.items) {
+        console.log(book.volumeInfo.title);
+        console.log(book.volumeInfo.subtitle);
+        //console.log(book.volumeInfo.authors[0]);
+    }
+
+
+    //create display of results
+    let element = document.createElement("p");
+    element.innerText = "test me";
+    searchResults.appendChild(element);
+}
+
+
 function Book() {
 
-    var listOfBooks = [];
-    //take in a search term
-    //return an object with results
-    async function searchTitle(title) {
-        console.log(`searching for title: ${title}`);
-
-        //GET https://www.googleapis.com/books/v1/volumes/volumeIdHere
-        //GET https://www.googleapis.com/books/v1/volumes?q=quilting
-        return fetch(`https://www.googleapis.com/books/v1/volumes?q=${title}`)
-            .then((res) => res.json())
-            .then((searchResults) => {
-                //console.log(searchResults);
-                //console.log(searchResults.items[0].volumeInfo.title);
-                //id
-                //data.items[0].id
-                //data.items[0].volumeInfo.title
-                //data.items[0].volumeInfo.subtitle
-                //data.items[0].volumeInfo.authors[]
-                //data.items[0].volumeInfo.description
-                //data.items[0].volumeInfo.averageRating
-                //data.items[0].volumeInfo.imageLinks.thumbnail
-                //console.log(searchResults.items);
-                //summarizeSearch(searchResults.items);
-                searchResults.items;
-
-            });
-    }
-
-    function summarizeSearch(searchResults) {
-        console.log(`in summarizeSearch: ${searchResults}`);
-        // let resultSummary = [];
-
-        // for (let i = 0; i < searchResults.length; i++) {
-        //     resultSummary.push(
-        //         {
-        //             id: searchResults[i].id,
-        //             title: searchResults[i].volumeInfo.title,
-        //             subtitle: searchResults[i].volumeInfo.subtitle,
-        //             authors: searchResults[i].volumeInfo.authors,
-        //             description: searchResults[i].volumeInfo.description,
-        //             averageRating: searchResults[i].volumeInfo.averageRating,
-        //             thumbnail: searchResults[i].volumeInfo.imageLinks.thumbnail
-        //         });
-        // }
-        // console.log(`Result Summary: ${resultSummary}`);
-        // return resultSummary;
-    }
 
 
     var bookAPI = {
@@ -60,17 +50,16 @@ function Book() {
         printFunny() {
             console.log("hahahah");
         },
-        search(searchTerm) {
-            searchTitle(searchTerm)
-                .then((seachResults) => {
-                    console.log(searchResults);
-                });
-
-
+        async search(searchTerm) {
+            let result = await searchSubject(searchTerm);
+            return result;
         }
     }
 
     return bookAPI;
 }
 
-module.exports = Book;
+if (!(typeof module === "undefined")) {
+    module.exports = { Book };
+}
+
