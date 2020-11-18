803c116279127f6f6f79401a7c47763f4f4d6e1c
Jake Simmens <rocknjake@gmail.com>
2020-11-17 21:28:55 -0600

    Set a standard data set to grab from Google API and pass along to the client

--- public/js/bookapi.js
+++ public/js/bookapi.js
@@ -9,10 +9,89 @@ function fetchBooks() {
 
         searchByTerm(term) {
             return fetchGoogle.searchByTerm(term);
+        },
+
+        testMe(bookId) {
+            return fetchGoogle.fetchById(bookId)
+                .then(bookData => formatGoogleData(bookData));
         }
+
     };
 
+    function formatGoogleData(data) {
+        let formatedData = {};
+
+        formatedData.id = data.id;
+        formatedData.title = data.volumeInfo.title;
+        formatedData.subtitle = data.volumeInfo.subtitle;
+        formatedData.authors = data.volumeInfo.authors;
+        formatedData.publisher = data.volumeInfo.publisher;
+        formatedData.publishedDate = data.volumeInfo.publishedDate;
+        formatedData.description = data.volumeInfo.description;
+        formatedData.pageCount = data.volumeInfo.pageCount;
+        formatedData.categories = data.volumeInfo.categories;
+        formatedData.averageRating = data.volumeInfo.averageRating;
+        formatedData.ratingsCount = data.volumeInfo.ratingsCount;
+        formatedData.imageLinks = data.volumeInfo.imageLinks;
+
+        return formatedData;
+    }
+
     return publicAPI;
+
+
+    // {
+    //     "kind": "books#volume",
+    //     "id": string,
+    //     "etag": string,
+    //     "selfLink": string,
+    //     "volumeInfo": {
+    //       "title": string,
+    //       "subtitle": string,
+    //       "authors": [
+    //         string
+    //       ],
+    //       "publisher": string,
+    //       "publishedDate": string,
+    //       "description": string,
+    //       "industryIdentifiers": [
+    //         {
+    //           "type": string,
+    //           "identifier": string
+    //         }
+    //       ],
+    //       "pageCount": integer,
+    //       "dimensions": {
+    //         "height": string,
+    //         "width": string,
+    //         "thickness": string
+    //       },
+    //       "printType": string,
+    //       "mainCategory": string,
+    //       "categories": [
+    //         string
+    //       ],
+    //       "averageRating": double,
+    //       "ratingsCount": integer,
+    //       "contentVersion": string,
+    //       "imageLinks": {
+    //         "smallThumbnail": string,
+    //         "thumbnail": string,
+    //         "small": string,
+    //         "medium": string,
+    //         "large": string,
+    //         "extraLarge": string
+    //       },
+    //       "language": string,
+    //       "previewLink": string,
+    //       "infoLink": string,
+    //       "canonicalVolumeLink": string
+    //     },
+    //     "searchInfo": {
+    //       "textSnippet": string
+    //     }
+    //   }
+
 }
 
 function fetchGoogleBooks() {
@@ -25,14 +104,11 @@ function fetchGoogleBooks() {
         fetchById(bookId) {
             return fetch(`${googleBookVolumesStr}${bookId}`)
                 .then(response => response.json());
-            //.then(data => console.log(data));
         },
 
         searchByTerm(term) {
             return fetch(`${googleBookQueryStr}${term}`)
                 .then(response => response.json());
-            //.then(data => console.log(data));
-            //.then(data => displaySearchResults(data));
         }
 
     };
