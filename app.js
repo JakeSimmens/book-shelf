f60822a1a030c0a1318c964cc9a97efa8bf961b4
Jake Simmens <rocknjake@gmail.com>
2020-11-25 15:56:30 -0600

    Extract details of formatting google book data from fetchGoogleBooks so it can be accessed for the book show page.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const axios = require("axios");
+
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -12,7 +14,12 @@ app.get('/', function getHome(req, res) {
 //show
 app.get('/book/:id', (req, res) => {
     //show a single book's details from google
-    res.send("test");
+    const { formatBookDataFromGoogle } = require("./public/js/extractBookData.js");
+
+    axios.get(`https://www.googleapis.com/books/v1/volumes/${req.params.id}`)
+        .then(response => formatBookDataFromGoogle(response.data))
+        .then(bookData => res.render("book", { bookData: bookData }))
+        .catch(err => console.log(`*****ERROR*****${err}`));
 });
 
 const port = process.env.PORT || 3000;
