214e561139c7befbc000fe51d33d01f38fd1ca5a
Jake Simmens <rocknjake@gmail.com>
2020-11-30 20:53:25 -0600

    Add post call when clicking 'Add Library' button on book page

--- app.js
+++ app.js
@@ -1,10 +1,67 @@
 const express = require("express");
 const app = express();
 const axios = require("axios");
+const bodyParser = require("body-parser");
 
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+//DATABASE
+var myLibrary = [
+    {
+        id: "abc123",
+        title: "The Book of One",
+        subtitle: "Prequel to Two",
+        authors: ["Bob Bill", "Ray Bob", "Tracy Fred"],
+        publisher: "Publishing House",
+        publishedDate: "July 2015",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla faucibus ligula leo. Morbi mattis diam vestibulum mi venenatis faucibus. Nullam nec euismod felis, a vulputate turpis. Nunc eget dui eget lorem mollis consectetur. Aliquam pharetra nunc vel eleifend sodales. Pellentesque eget ex ac nisl fringilla vulputate. Integer aliquam ultrices felis at elementum. Vestibulum libero massa, eleifend a libero et, auctor ultricies odio. Donec pretium tincidunt diam, et malesuada ex tincidunt sed.",
+        pageCount: 590,
+        categories: "adventure",
+        averageRating: 4.7,
+        ratingsCount: 14698,
+        imageLinks: {
+            thumbnail: "/images/hobbit.jpg",
+            small: "/images/hobbit.jpg"
+        }
+    },
+    {
+        id: "def123",
+        title: "The Book of Two",
+        subtitle: "Prequel to Three",
+        authors: ["One Bob", "Four Fred"],
+        publisher: "What? Publishing House",
+        publishedDate: "May 2018",
+        description: "A Second book. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla faucibus ligula leo. Morbi mattis diam vestibulum mi venenatis faucibus. Nullam nec euismod felis, a vulputate turpis. Nunc eget dui eget lorem mollis consectetur. Aliquam pharetra nunc vel eleifend sodales. Pellentesque eget ex ac nisl fringilla vulputate. Integer aliquam ultrices felis at elementum. Vestibulum libero massa, eleifend a libero et, auctor ultricies odio. Donec pretium tincidunt diam, et malesuada ex tincidunt sed.",
+        pageCount: 450,
+        categories: "romantic",
+        averageRating: 2.1,
+        ratingsCount: 589,
+        imageLinks: {
+            thumbnail: "/images/lostWorld.jpg",
+            small: "/images/lostWorld.jpg"
+        }
+    },
+    {
+        id: "last123",
+        title: "Three Book",
+        subtitle: "Another one",
+        authors: ["One Bob"],
+        publisher: "House of Books",
+        publishedDate: "May 2018",
+        description: "A third book. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla faucibus ligula leo. Morbi mattis diam vestibulum mi venenatis faucibus. Nullam nec euismod felis, a vulputate turpis. Nunc eget dui eget lorem mollis consectetur. Aliquam pharetra nunc vel eleifend sodales. Pellentesque eget ex ac nisl fringilla vulputate. Integer aliquam ultrices felis at elementum. Vestibulum libero massa, eleifend a libero et, auctor ultricies odio. Donec pretium tincidunt diam, et malesuada ex tincidunt sed.",
+        pageCount: 234,
+        categories: "sleeper",
+        averageRating: 2.3,
+        ratingsCount: 45,
+        imageLinks: {
+            thumbnail: "/images/darkTower.jpg",
+            small: "/images/darkTower.jpg"
+        }
+    }
+];
+
 
 app.get('/', function getHome(req, res) {
 
@@ -24,6 +81,13 @@ app.get('/book/:id', (req, res) => {
 
 app.post('/book', (req, res) => {
     console.log("post request to add book");
+    const googleBookID = req.body.bookID;
+    axios.get(`https://www.googleapis.com/books/v1/volumes/${googleBookID}`)
+        .then(response => formatBookDataFromGoogle(response.data))
+        .then(bookData => console.log(bookData))
+        .catch(err => console.log(`*****ERROR*****${err}`));
+
+    //need to add retrieved data to the library and database
 
     res.redirect("/");
 });
