07b05b1a7e37823df5bc4c5a595b48b0bae48435
Jake Simmens <rocknjake@gmail.com>
2021-01-15 18:16:54 -0600

    Add tests for accessing all routes in index.js

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,6 +1,6 @@
 const testApp = require("../app");
 const request = require("supertest");
-const databaseAPI = require("../database");
+const bodyParser = require("body-parser");
 
 jest.mock("../seed.js");  //stop seed from running
 jest.mock("../database", () => {
@@ -27,13 +27,47 @@ describe("Route - index.js", () => {
     });
 
     it("should load the homepage", async () => {
-        let response = await request(testApp)
+        await request(testApp)
             .get("/")
- 
-        expect(response.statusCode).toEqual(200);
+            .expect(200)
     });
 
-    it("should work on this test", () => {
-        expect(2+5).toBe(7);
+    describe("Routes for /register", () => {
+        it("should GET register page", async() => {
+            await request(testApp)
+                .get("/register")
+                .expect(200)
+        });
+    
+        it("should POST register page with no credentials sent", async() => {
+            await request(testApp)
+                .post("/register")
+                .expect(302)
+        });
     });
+
+    describe("Routes for /login", () => {
+        it("should GET login", async () => {
+            await request(testApp)
+                .get("/login")
+                .expect(200)
+        });
+    
+        it("should POST login with no credentials sent", async () => {
+            await request(testApp)
+                .post("/login")
+                .expect(302)
+        });
+    });
+
+    describe("Route for /logout", () => {
+        it("should GET /logout with no one logged in", async () => {
+            await request(testApp)
+                .get("/logout")
+                .expect(302)
+        });
+    });
+
+
+
 });
