d12b7cb148a3ae56ca522208da1f68faa9642ebe
Jake Simmens <rocknjake@gmail.com>
2023-07-19 17:21:04 -0500

    Add star counter to book.ejs and starCounter modifier to bookPage.js when clicking on stars.

--- public/js/bookPage.js
+++ public/js/bookPage.js
@@ -1,50 +1,58 @@
+const starCount = document.querySelector(".star-counter")
 const oneStarReview = document.querySelector(".one-star")
 const twoStarReview = document.querySelector(".two-star")
 const threeStarReview = document.querySelector(".three-star")
 const fourStarReview = document.querySelector(".four-star")
 const fiveStarReview = document.querySelector(".five-star")
 
-oneStarReview.addEventListener("click", 
+if(oneStarReview){
+  oneStarReview.addEventListener("click", 
   function fillOneStar(){
     oneStarReview.textContent = "\u2605"
     twoStarReview.textContent = "\u2729"
     threeStarReview.textContent = "\u2729"
     fourStarReview.textContent = "\u2729"
     fiveStarReview.textContent = "\u2729"
+    starCount.value = 1
   })
 
 twoStarReview.addEventListener("click", 
-  function fillOneStar(){
+  function fillTwoStars(){
     oneStarReview.textContent = "\u2605"
     twoStarReview.textContent = "\u2605"
     threeStarReview.textContent = "\u2729"
     fourStarReview.textContent = "\u2729"
     fiveStarReview.textContent = "\u2729"
+    starCount.value = 2
   })
 
 threeStarReview.addEventListener("click", 
-  function fillOneStar(){
+  function fillThreeStars(){
     oneStarReview.textContent = "\u2605"
     twoStarReview.textContent = "\u2605"
     threeStarReview.textContent = "\u2605"
     fourStarReview.textContent = "\u2729"
     fiveStarReview.textContent = "\u2729"
+    starCount.value = 3
   })
 
 fourStarReview.addEventListener("click", 
-  function fillOneStar(){
+  function fillFourStars(){
     oneStarReview.textContent = "\u2605"
     twoStarReview.textContent = "\u2605"
     threeStarReview.textContent = "\u2605"
     fourStarReview.textContent = "\u2605"
     fiveStarReview.textContent = "\u2729"
+    starCount.value = 4
   })
 
 fiveStarReview.addEventListener("click", 
-  function fillOneStar(){
+  function fillFiveStars(){
     oneStarReview.textContent = "\u2605"
     twoStarReview.textContent = "\u2605"
     threeStarReview.textContent = "\u2605"
     fourStarReview.textContent = "\u2605"
     fiveStarReview.textContent = "\u2605"
+    starCount.value = 5
   })
+}
